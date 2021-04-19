593cb23f96599957342ba0827be5234ed98c845c
LeksymonIT <LeksymonIT@gmail.com>
2021-04-19 20:56:27 +0300

    ADD

--- MethodNewmarka.py
+++ MethodNewmarka.py
@@ -4,14 +4,14 @@ import numpy as np
 
 
 def func(u, ut, t):
-    return 0  # -w2 * u - 2.0 * beta * ut + a * math.sin(omega * t)
+    return a * math.sin(omega * h * t)
 
 
 def newmark(dt, m, alpha, c):
     CONST = 1 / (m / (a1 * dt * dt) + (alpha * a2) / (a1 * dt) + c)
 
-    u = 0.01
-    u_t = 0.02
+    u = 0
+    u_t = 0
     u_2t = -(alpha * u_t) / m - (c * u) / m + k * func(u, u_t, 0) / m
 
     result_u.append([u])
@@ -21,7 +21,7 @@ def newmark(dt, m, alpha, c):
     for i in range(int(n) - 1):
         u_n = CONST * m * (u / (a1 * dt * dt) + u_t / (a1 * dt) - u_2t * (1 - 1 / (2 * a1))) \
               + CONST * alpha * ((a2 * u) / (a1 * dt) + u_t * (1 - a2 / a1) - dt / 2 * (2 - a2 / a1) * u_2t) \
-              + func(u, u_t, i) * CONST
+              + func(u, u_t, i+1) * CONST
         u_t_n = a2 * (u_n - u) / (a1 * dt) + (1 - a2 / a1) * u_t + u_2t * dt * (2 - a2 / a1) / 2
         u_2t_n = (u_n - u) / (a1 * dt * dt) - u_t / (a1 * dt) + (1 - 1 / (2 * a1)) * u_2t
 
@@ -40,15 +40,15 @@ result_u_2t = []
 
 a1 = 0.25
 a2 = 0.50
-k = 5
-m = 1.2  # 5
+k = 1
+m = 5  # 5
 a = k / m
-c = 2.1  # 20
-alpha = 0.23
+c = 300  # 20
+alpha = 0
 beta = alpha / (2 * m)
 w2 = c / m
 w = math.sqrt(w2)
-omega = 2.2  # w-0.01
+omega = 0.01  # w-0.01
 
 t_start = 0
 t_end = 10
@@ -57,13 +57,11 @@ h = (t_end - t_start) / n
 
 newmark(h, m, alpha, c)
 t_array = []
-
+w
 for i in range(int(n)):
     t_array.append(h * i)
 
 fig, ax = plt.subplots()
 ax.plot(t_array, result_u)
-ax.plot(t_array, result_u_t)
-ax.plot(t_array, result_u_2t)
 
 plt.show()
